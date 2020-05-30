bce059efa4977b793d41eb8a00e89746c6b49f7f
Wesley Alves <wesleyalves94sv@hotmail.com>
2020-05-30 16:39:39 -0300

    initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import SEO from '../components/seo'
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <SEO title="404: Página não encontrada" />
+      <h1>Página não encontrada</h1>
+    </>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import SEO from '../components/seo'
+
+export default function HomePage() {
+  return (
+    <>
+      <SEO title="Home" />
+      <h1>Home</h1>
+    </>
+  )
+}
