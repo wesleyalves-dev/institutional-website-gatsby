bf99ef4fe60382829767034d0c20276be5e88df0
Wesley Alves <wesleyalves94sv@hotmail.com>
2020-06-28 11:40:46 -0300

    404 page

---
+++ src/components/NotFound/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  width: 90%;
+  max-width: 800px;
+  margin: 200px auto;
+
+  p {
+    a {
+      font-weight: 900;
+    }
+  }
+`
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 import SEO from '../components/seo'
 import Layout from '../components/Layout'
+import NotFound from '../components/NotFound'
 
 export default function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Página não encontrada" />
-      <h1>Página não encontrada</h1>
+      <NotFound />
     </Layout>
   )
 }
